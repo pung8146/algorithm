eb1189c1542e8ed73323c976dc8ffc517922505f
pung8146 <pung8146@naver.com>
2022-08-24 19:34:40 +0900

    백준 2588 세자리수 의 곱셈 문제

--- backjun/입출력과사칙연산/2588.js
+++ backjun/입출력과사칙연산/2588.js
@@ -9,7 +9,20 @@
 // 출력
 // 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
 
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString();
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
+const A = Number(input[0]);
+const B = input[1];
+
+let num3 = A * Number(B[2]);
+let num4 = A * Number(B[1]);
+let num5 = A * Number(B[0]);
+let num6 = A * Number(B);
+
+
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
 //
